ed1a85f4a0ad0c447f3e09027d7ae9c08a8adc69
David Juarez <d.juarez@dkfz-heidelberg.de>
2026-04-24 14:55:57 +0200

    chore: parameters for sending query to bridgehead

--- src/main/java/de/samply/query/QueryEventService.java
+++ src/main/java/de/samply/query/QueryEventService.java
@@ -1,8 +1,10 @@
 package de.samply.query;
 
+import de.samply.app.ProjectManagerConst;
 import de.samply.exporter.ExporterJob;
 import de.samply.project.ProjectBridgeheadService;
 import de.samply.project.SendQueryToBridgeheadEvent;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -12,13 +14,19 @@ public class QueryEventService {
 
     private final ProjectBridgeheadService projectBridgeheadService;
     private final ExporterJob exporterJob;
+    private final int sleepTime;
+    private final int maxNumberOfRetries;
 
 
     public QueryEventService(
             ProjectBridgeheadService projectBridgeheadService,
-            ExporterJob exporterJob) {
+            ExporterJob exporterJob,
+            @Value(ProjectManagerConst.TIME_BETWEEN_CHECK_EXPORTS_IN_SECONDS_SV) int sleepTime,
+            @Value(ProjectManagerConst.MAX_NUMBER_OF_RETRIES_BETWEEN_CHECK_EXPORTS_SV) int maxNumberOfRetries) {
         this.projectBridgeheadService = projectBridgeheadService;
         this.exporterJob = exporterJob;
+        this.sleepTime = sleepTime;
+        this.maxNumberOfRetries = maxNumberOfRetries;
     }
 
     /**
@@ -34,9 +42,9 @@ public class QueryEventService {
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void onQuerySentToBridgehead(@SuppressWarnings("unused") SendQueryToBridgeheadEvent event) throws InterruptedException {
         // Execute twice to have enough time to check if the query has arrived to the bridgehead
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < maxNumberOfRetries; i++) {
             exporterJob.checkExports();
-            Thread.sleep(5000);
+            Thread.sleep(this.sleepTime * 1000L);
         }
     }
 
